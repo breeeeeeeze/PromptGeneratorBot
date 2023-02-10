5883721525ba82eb609a2b681c29810d5442f775
niboeing <32905101+breeeeeeeze@users.noreply.github.com>
2023-02-10 20:29:38 +0100

    Improved api errors

--- src/utils/apiHelpers.ts
+++ src/utils/apiHelpers.ts
@@ -13,8 +13,10 @@ export async function postRequest(url: string, body: object) {
     .catch((error) => {
       container.logger.error(error);
       const error_message = error?.response
-        ? `${error.response?.status} ${error.response?.statusText}`
+        ? `${error.response?.status}`
+        : error.code == 'ECONNREFUSED'
+        ? "API couldn't be reached and probably isn't running. Ping <@211481842312806400>"
         : `${error.code}`;
-      return `[API ERROR] ${error_message}`;
+      return `API Error: ${error_message}`;
     });
 }
