272cf9038f0c03048037d9f5d8e4325a4783f383
niboeing <32905101+breeeeeeeze@users.noreply.github.com>
2023-02-10 11:50:54 +0100

    made url validation case insensitive

--- src/utils/validateURL.ts
+++ src/utils/validateURL.ts
@@ -1,5 +1,5 @@
 export function validateURL(url: string) {
-  const imageExtensionRegex = /\.(jpeg|jpg|gif|png|webp)$/;
+  const imageExtensionRegex = /\.(jpeg|jpg|gif|png|webp)$/i;
   const urlRegex =
     /^(https?:\/\/)(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)$/;
   return urlRegex.test(url) && imageExtensionRegex.test(url);
